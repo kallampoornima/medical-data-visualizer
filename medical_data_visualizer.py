44e0afda4ddf27d0bb67794810cc59d678e9e3a3
kallampoornima <kallampoornima@gmail.com>
2025-12-17 20:29:01 +0000

    until heatmap

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -45,4 +45,20 @@ def draw_cat_plot():
     
 
 
-    
+    # 9
+    fig.savefig('catplot.png')
+    return fig
+
+
+# 10
+def draw_heat_map():
+    # 11
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) &
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
+
+
